e8196ac7cafe1ad9db568286c0675ad8cbdd37e8
Lyndisdecaelin <guillaume.bessueille@isen.yncrea.fr>
2020-02-18 09:09:13 -0500

    init projet

---
+++ tp_serial/tp_serial/MyJSON.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class MyJSON
+{
+    public static Dictionary<String, dynamic> serialize(dynamic o)
+    {
+        Type type = o.GetType();
+        Dictionary<String, dynamic> json = new Dictionary<string, dynamic>();
+        var prop = type.GetProperties(System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+        foreach (var p in prop)
+        {
+            Console.WriteLine(p);
+        }
+        return null;
+    }
+
+}
---
+++ tp_serial/tp_serial/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace tp_serial
+{
+
+    public class toto
+    {
+        public int i = 42;
+        private String name = "mon nom";
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            toto A = new toto();
+            MyJSON.serialize(A);
+        }
+    }
+}
