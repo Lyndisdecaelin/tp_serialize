15c836b7d907cde62743c1597aa2c06fe26ae4e2
Lyndisdecaelin <guillaume.bessueille@isen.yncrea.fr>
2020-02-18 10:53:25 -0500

    exo 1 avance

--- tp_serial/tp_serial/MyJSON.cs
+++ tp_serial/tp_serial/MyJSON.cs
@@ -1,18 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
 
 public class MyJSON
 {
+
+    public static dynamic iter(FieldInfo p,dynamic o){
+        dynamic res;
+        if(p.FieldType.IsPrimitive || p.FieldType == typeof(String)){
+                res = p.GetValue(o);
+            }
+            else if(typeof(System.Collections.IEnumerable).IsAssignableFrom(p.FieldType)) {
+                res = new List<dynamic>();
+                foreach(var elem in p.GetValue(o)){
+                    res.add(iter(elem.GetType().GetField(), elem));
+                }
+            }
+            else{
+                res = serialize(p.GetValue(o));
+            }
+
+        return res;
+    }
     public static Dictionary<String, dynamic> serialize(dynamic o)
     {
         Type type = o.GetType();
         Dictionary<String, dynamic> json = new Dictionary<string, dynamic>();
-        var prop = type.GetProperties(System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+        var prop = type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
         foreach (var p in prop)
         {
-            Console.WriteLine(p);
+            json[p.Name] = iter(p,o);
         }
-        return null;
+        return json;
     }
 
 }
--- tp_serial/tp_serial/Program.cs
+++ tp_serial/tp_serial/Program.cs
@@ -1,19 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace tp_serial
 {
 
+    public class titi
+    {
+        String name = "titiiiiiii";
+    }
     public class toto
     {
-        public int i = 42;
+        public short i = 42;
+        public titi B = new titi();
+
+        private List<String> table = new List<String>{"un","deux","trois" };
+        
         private String name = "mon nom";
+
+        private bool yes = true;
     }
     class Program
     {
         static void Main(string[] args)
         {
             toto A = new toto();
-            MyJSON.serialize(A);
+            var json = MyJSON.serialize(A);
+            Console.WriteLine("fin");
         }
     }
 }
